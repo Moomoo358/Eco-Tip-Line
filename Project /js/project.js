6ec6f352aef102e9a9a5d01d4309c24619142f31
Alise Morgan <91515842+amoma222@users.noreply.github.com>
2021-11-18 15:32:46 -0800

    Changes

---
+++ Project /js/project.js
@@ -0,0 +1,19 @@
+
+
+//Add cick event to challenge button
+  $("#Household").click(function(){
+    $(this).parent().toggleClass(“special”);
+})
+
+//Add click event to problems button
+  $("#Transportation").click(function(){
+    $(this).parent().toggleClass("special");
+})
+
+$("#Health/Wellness").click(function(){
+  $(this).parent().toggleClass(“special”);
+})
+
+$("#Food").click(function(){
+  $(this).parent().toggleClass(“special”);
+})
