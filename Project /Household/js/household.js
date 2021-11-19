b312a3ef4bd53234e5545e6dc8dcc68990ccb99e
Alise Morgan <91515842+amoma222@users.noreply.github.com>
2021-11-19 14:34:18 -0800

    Update household.js

--- Project /Household/js/household.js
+++ Project /Household/js/household.js
@@ -1,19 +1,1 @@
 
-
-//Add cick event to challenge button
-  $("#Household").click(function(){
-    $(this).parent().toggleClass(“special”);
-})
-
-//Add click event to problems button
-  $("#Transportation").click(function(){
-    $(this).parent().toggleClass("special");
-})
-
-$("#Health/Wellness").click(function(){
-  $(this).parent().toggleClass(“special”);
-})
-
-$("#Food").click(function(){
-  $(this).parent().toggleClass(“special”);
-})
