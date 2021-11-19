ec71484bea1f66323193d61bdf5c935eda506b06
Alise Morgan <91515842+amoma222@users.noreply.github.com>
2021-11-19 14:29:40 -0800

    Changes to Javascript on Homepage

--- Project /js/project.js
+++ Project /js/project.js
@@ -1,3 +1,19 @@
+//Add cick event to Household button
+  $("#Household").click(function(){
+    $(this).parent().toggleClass(“special”);
+})
 
-var button = document.getElementById('Image');
-button.addEventListener('click', alertClick);
+//Add click event to Transportation button
+  $("#Transportation").click(function(){
+    $(this).parent().toggleClass("special");
+})
+
+//Add click event to Health/Wellness button
+$("#Health/Wellness").click(function(){
+  $(this).parent().toggleClass(“special”);
+})
+
+//Add click event to Food button
+$("#Food").click(function(){
+  $(this).parent().toggleClass(“special”);
+})
