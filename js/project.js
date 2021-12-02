3a2ef9f7e3b38d91b44525699ee51ad94630161d
Alise Morgan <91515842+amoma222@users.noreply.github.com>
2021-12-01 18:17:57 -0800

    Update project.js

--- js/project.js
+++ js/project.js
@@ -1,1 +1,5 @@
 
+var button = document.getElementById('my-button');
+button.addEventListener('click', function() {
+    inputValue = document.getElementById('my-input').value;
+    console.log("You input:", inputValue);
