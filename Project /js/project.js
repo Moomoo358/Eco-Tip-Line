29a85185a0aad2ee71901bc747dbe15b5883ac40
Monique Salinas <mnsalina@ucsc.edu>
2021-12-05 20:47:39 -0800

    more updates

--- Project /js/project.js
+++ Project /js/project.js
@@ -1,22 +1,22 @@
-//
-// var tip-options function (){
-//   if ("Household") {
-//     console.log(data);
-//
-//   } else if ("Transportation") {
-//       console.log(data);
-//
-//   } else if ("Health") {
-//       console.log(data);
-//
-//   } else if ("Food")
-//     console.log(data)
-//
-//
-// var button = document.getElementById('submit');
-// button.addEventListener('click', function() {
-//     inputValue = document.getElementById('enter-tip').value;
-//     console.log("You input:", inputValue);
+
+var tip-options function (){
+  if ("Household") {
+    console.log(data);
+
+  } else if ("Transportation") {
+      console.log(data);
+
+  } else if ("Health") {
+      console.log(data);
+
+  } else if ("Food")
+    console.log(data)
+
+
+var button = document.getElementById('submit');
+button.addEventListener('click', function() {
+    inputValue = document.getElementById('enter-tip').value;
+    console.log("You input:", inputValue);
 
   $("#submit").click(function() {
 
