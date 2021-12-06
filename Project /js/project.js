4a72c7cdd768d9d23935aff216396a569f455e48
Monique Salinas <mnsalina@ucsc.edu>
2021-12-06 07:30:25 -0800

    Update project.js

--- Project /js/project.js
+++ Project /js/project.js
@@ -1,38 +1,38 @@
 
-// var tip-options function (){
-//   if ("Household") {
-//     console.log(data);
-//
-//   } else if ("Transportation") {
-//       console.log(data);
-//
-//   } else if ("Health") {
-//       console.log(data);
-//
-//   } else if ("Food")
-//     console.log(data)
-//
-//
-// var button = document.getElementById('submit');
-// button.addEventListener('click', function() {
-//     inputValue = document.getElementById('enter-tip').value;
-//     console.log("You input:", inputValue);
+var tip-options function (){
+  if ("Household") {
+    console.log(data);
 
-  $("#submit").click(function() {
+  } else if ("Transportation") {
+      console.log(data);
 
-    if ($("#tip-options").val() == "household") {
-      window.location.href = "household/index.html";
-    }
+  } else if ("Health") {
+      console.log(data);
 
-    else if ($("#tip-options").val() == "transportation") {
-      window.location.href = "transportation/index.html";
-    }
+  } else if ("Food")
+    console.log(data)
 
-    else if ($("#tip-options").val() == "health") {
-      window.location.href = "health/index.html";
-    }
 
-    else if ($("#tip-options").val() == "food") {
-      window.location.href = "food/index.html";
-    }
-});
+var button = document.getElementById('submit');
+button.addEventListener('click', function() {
+    inputValue = document.getElementById('enter-tip').value;
+    console.log("You input:", inputValue);
+
+//   $("#submit").click(function() {
+//
+//     if ($("#tip-options").val() == "household") {
+//       window.location.href = "household/index.html";
+//     }
+//
+//     else if ($("#tip-options").val() == "transportation") {
+//       window.location.href = "transportation/index.html";
+//     }
+//
+//     else if ($("#tip-options").val() == "health") {
+//       window.location.href = "health/index.html";
+//     }
+//
+//     else if ($("#tip-options").val() == "food") {
+//       window.location.href = "food/index.html";
+//     }
+// });
